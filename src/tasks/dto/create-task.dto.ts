8446e5206603941bdaa17833fefc54ad00c0d64c
davepm <davidmendoza911119@gmail.com>
2025-03-23 23:43:32 -0600

    feature: update Task model the way that status is optional

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,4 @@
 import { IsString, MinLength, IsEnum } from "class-validator"
-import { TaskStatus } from "@prisma/client";
 
 const MIN_LENGTH = 1;
 export class CreateTaskDto {
@@ -11,7 +10,4 @@ export class CreateTaskDto {
   @IsString()
   @MinLength(MIN_LENGTH)
   description: string;
-
-  @IsEnum(TaskStatus, { message: 'Status must be one of PENDING, ON_HOLD, IN_PROGRESS, UNDER_REVIEW, COMPLETED' })
-  status: TaskStatus;
 }
